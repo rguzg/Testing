56be2719991d3143414f69830918151eea9281e7
Raúl <raulguzmanguerrero@outlook.com>
2021-06-14 19:53:02 -0500

    Add matchers.test.js

---
+++ __test__/matchers.test.js
@@ -0,0 +1,21 @@
+import { describe, expect, test } from '@jest/globals'
+
+describe('Comparadores comunes', () => {
+    let user = {
+        name: "Raúl",
+        lastname: "Berraizaba"
+    };
+
+    let user2 = {
+        name: "Raúl",
+        lastname: "Berraizaba"
+    };
+
+    test('Igualdad de elementos', () => {
+        expect(user).toEqual(user2)
+    });
+
+    test('No son exactamente iguales', () => {
+        expect(user).not.toEqual(user2)
+    });
+});
